471e91ce6261b47b831464a79ad65fd8d7c6bb0d
Zach Daniel <zach@zachdaniel.dev>
2025-05-23 23:38:03 -0400

    chore: release version v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule UsageRules.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
   @description """
   A dev tool for Elixir projects to gather LLM usage rules from dependencies
   """
@@ -10,7 +11,7 @@ defmodule UsageRules.MixProject do
   def project do
     [
       app: :usage_rules,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       package: package(),
       aliases: aliases(),
@@ -90,7 +91,7 @@ defmodule UsageRules.MixProject do
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:git_ops, "~> 2.5", only: [:dev, :test]}
+      {:git_ops, "~> 2.0", only: [:dev]}
     ]
   end
 end
