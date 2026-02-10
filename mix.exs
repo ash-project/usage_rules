e475351ec6582e785c366eb1bf5ef0976440d554
Zach Daniel <zach@zachdaniel.dev>
2026-02-10 00:11:04 -0500

    chore: release 1.0.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@
 defmodule UsageRules.MixProject do
   use Mix.Project
 
-  @version "1.0.1"
+  @version "1.0.2"
   @description """
   A config-driven dev tool for Elixir projects to manage AGENTS.md files and agent skills from dependencies
   """
